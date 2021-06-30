d00d313c570e7eaf26b0407b0fb6b2d303ad74dd
Araceli Corley <abcorley@bu.edu>
2021-06-30 17:08:24 +0000

    initial commit

---
+++ Weather_API.py
@@ -0,0 +1,31 @@
+import requests
+import json 
+
+BASE_URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'
+
+API_Key = 'LCRCQE8SWKLGXT6837WYWGATZ'
+
+Location = 'New York City'
+
+Date1 = '2020-07-01'
+
+Date2 = '2020-07-02'
+
+"""for 15 day forcatse instead of Date1 and Date2 use timeline"""
+
+response = requests.get(BASE_URL + Location + '/' + Date1 + '/' + Date2 + '?key=' + API_Key )
+data = response.json()
+
+for key, value in data.items():
+  print(key, ':', value)
+
+"""Days key includes daily info:
+      datetime: yyyy-mm-dd
+      temp,
+      feelslike,
+      precipitation,
+      etc.
+"""
+
+for weather in data['days']:
+  print(weather['datetime'], weather['temp'])
