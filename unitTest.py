f5860fa1ce29a6d8430f299aae7c65d84bd1ebe8
Araceli Corley <abcorley@bu.edu>
2021-06-30 22:05:08 +0000

    Style Changes

--- unitTest.py
+++ unitTest.py
@@ -5,14 +5,17 @@ from Weather_API import get_info, create_URL, get_data
 class unitTest(unittest.TestCase):
     def test_get_info(self):
         key, location = get_info()
-        self.assertEqual(len(key),25)
+        self.assertEqual(len(key), 25)
         self.assertNotEquals(location, '')
+    
+    
     def test_get_data(self):
         key, location = get_info()
         url = create_URL(key, location)
         response = get_data(url)
-        self.assertEqual(response.status_code,200)
-        
+        self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
+    
