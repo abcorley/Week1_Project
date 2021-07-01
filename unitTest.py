193582b5379dce8f0fef55efe53d484127d0d6f1
Araceli Corley <abcorley@bu.edu>
2021-07-01 16:39:25 +0000

    Removed user input and set defaults

--- unitTest.py
+++ unitTest.py
@@ -4,12 +4,14 @@ from Weather_API import get_info, create_URL, get_data
 
 class unitTest(unittest.TestCase):
     def test_get_info(self):
-        key, location = get_info()
+        key = 'GDVTJYUO5ESFVH739JNG54OL0'
+        location = 'Houston'
         self.assertEqual(len(key), 25)
         self.assertNotEquals(location, '')
 
     def test_get_data(self):
-        key, location = get_info()
+        key = 'GDVTJYUO5ESFVH739JNG54OL0'
+        location = 'Houston'
         url = create_URL(key, location)
         response = get_data(url)
         self.assertEqual(response.status_code, 200)
